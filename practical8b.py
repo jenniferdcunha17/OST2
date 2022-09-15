69aea9ae51a21d4bfaa7c5746afd2966b91196aa
jenniferdcunha17 <113586302+jenniferdcunha17@users.noreply.github.com>
2022-09-15 13:41:29 +0530

    Add files via upload

---
+++ practical8b.py
@@ -0,0 +1,20 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\Lenovo\Downloads\AIPract8a.py ===============
+Running time:0.0ms
+[('C', 'Table'), ('B', 'E'), ('C', 'A')]
+Running time:0.0ms
+[('E', 'Table'), ('D', 'C'), ('E', 'D')]
+Running time:0.0ms
+[('C', 'Table'), ('B', 'Table'), ('A', 'E'), ('B', 'A'), ('C', 'B')]
+Running time:1.0001659393310547ms
+[('C', 'Table'), ('B', 'Table'), ('E', 'Table'), ('A', 'E'), ('D', 'C'), ('B', 'A')]
+Running time:0.9992122650146484ms
+[('F', 'Table'), ('E', 'Table'), ('D', 'C'), ('E', 'D'), ('F', 'E'), ('I', 'Table'), ('H', 'Table'), ('G', 'F'), ('H', 'G'), ('I', 'H')]
+Running time:4.012107849121094ms
+[('C', 'Table'), ('B', 'Table'), ('F', 'Table'), ('E', 'Table'), ('I', 'Table'), ('H', 'I'), ('G', 'H'), ('F', 'G'), ('E', 'F'), ('D', 'E'), ('C', 'D'), ('B', 'C'), ('A', 'B')]
+Running time:4.996299743652344ms
+[('C', 'Table'), ('B', 'Table'), ('F', 'Table'), ('E', 'Table'), ('D', 'B'), ('I', 'Table'), ('H', 'Table'), ('E', 'H'), ('F', 'E'), ('A', 'F'), ('C', 'A'), ('I', 'G')]
+Running time:2.0051002502441406ms
+[('C', 'Table'), ('B', 'Table'), ('F', 'Table'), ('E', 'B'), ('D', 'F'), ('C', 'D'), ('I', 'C'), ('H', 'E'), ('G', 'I'), ('A', 'G')]
